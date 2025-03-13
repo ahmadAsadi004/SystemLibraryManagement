d7faafcb32560b93a594352a47bbe26b838fbfcc
Ahmad <161066945+ahmadAsadi004@users.noreply.github.com>
2025-03-13 12:47:56 +0330

    System Library Management v2

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        var User1 = new Users(
-                "Ahmad",
-                21,
-                "male",
-                "ahmad@gmail.com",
-                "09152567092",
-                "Behzisti",
-                1402
-        );
 
-        var User2 = new Users(
-                "Mmd",
-                21,
-                "male",
-                "ahmad@gmail.com",
-                "09152567092",
-                "Behzisti",
-                1402
-        );
 
         System.out.println(User1.getId());
         System.out.println(User2.getId());
--- src/ManageUsers.java
+++ src/ManageUsers.java
@@ -1,6 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class ManageUsers {
+    private static Map<String, Users> users = new HashMap<>();
 
+    public static void addUser(String name, int age, String gender, int joinTime) {
+        String id = ManageID.CreateID(joinTime);
+        var newUser = new Users(name, age, gender, joinTime, id);
+        boolean exists = users.values().stream().anyMatch(user -> user.equals(newUser));
+        if (!exists) {
+            users.put(id, newUser);
+            System.out.println("User " + name + " has been created");
+        } else System.out.println("User " + name + " already exists");
+    }
 
+    public static Users searchUser(String id) {
+        if (users.containsKey(id))
+            return users.get(id);
+        System.out.println("User " + id + " does not exist");
+        return null;
+    }
 
+    public static void removeUser(String id) {
+        if (users.containsKey(id)) {
+            users.remove(id);
+            System.out.println("User " + id + " removed");
+        } else System.out.println("User " + id + " does not exist");
+    }
 
+    public static Users getUser(String id) {
+        return users.get(id);
+    }
 }
